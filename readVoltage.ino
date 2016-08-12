4f8a976c00ac9a00225ccdfae4d78042b42c107d
dakodako <chididi_gloria@hotmail.com>
2016-08-12 15:30:49 +1000

    micros() added

--- readVoltage.ino
+++ readVoltage.ino
@@ -1,12 +1,17 @@
 int sensorPin = A0;
 int sensorValue = 0;
+unsigned long time;
 void setup() {
   // put your setup code here, to run once:
  Serial.begin(9600);
+ 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  Serial.print("Time: ");
+  time = micros();
+  Serial.println(time);
   sensorValue = analogRead(sensorPin);
   Serial.println(sensorValue);
 }
