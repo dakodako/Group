71315715389630de94a3ff9158470a8e5cc48dbc
dakodako <chididi_gloria@hotmail.com>
2016-08-13 01:53:33 +1000

    Add files via upload
    add temperature reading

--- readVoltage.ino
+++ readVoltage.ino
@@ -1,5 +1,7 @@
 int sensorPin = A0;
 int sensorValue = 0;
+int mappedValue = 0;
+#define resistor 5000
 unsigned long time;
 void setup() {
   // put your setup code here, to run once:
@@ -9,9 +11,11 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  Serial.print("Time: ");
-  time = micros();
-  Serial.println(time);
+  //time = micros();
   sensorValue = analogRead(sensorPin);
+  //mappedValue = map(sensorValue,0,1023,-55,150);
+  float voltage = sensorValue * (5.0 / 1023.0);
+  float temperature = (voltage/resistor) * (10^6) + 273.15;
   Serial.println(sensorValue);
+  Serial.println(voltage);
 }
