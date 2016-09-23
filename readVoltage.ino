a5170b88311ce7c3ccf281dfa636bc7d1539ed12
dakodako <chididi_gloria@hotmail.com>
2016-09-23 15:58:47 +1000

    Add files via upload

--- readVoltage.ino
+++ readVoltage.ino
@@ -1,5 +1,7 @@
-int sensorPin = A0;
+//int sensorPin = A1;
+int sensorPin2 = A2;
 int sensorValue = 0;
+int sensorValue2 = 0;
 int mappedValue = 0;
 #define resistor 5000
 unsigned long time;
@@ -12,10 +14,21 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
   //time = micros();
-  sensorValue = analogRead(sensorPin);
+  //sensorValue = analogRead(sensorPin);
+  sensorValue2 = analogRead(sensorPin2);
   //mappedValue = map(sensorValue,0,1023,-55,150);
-  float voltage = sensorValue * (5.0 / 1023.0);
-  float temperature = voltage * 200 + 273.15;
-  Serial.println(sensorValue);
-  Serial.println(voltage);
+ // float voltage = sensorValue * (5.0 / 1023.0);
+  float voltageAD = (sensorValue2*1.0)/1024 * 5.0;
+  float temperature = (voltageAD) * 100.0 - 273.15;
+ // float resistance = (voltage*60000.0)/(5-voltage);
+ // float temperature = voltage * 200 + 273.15;
+  //Serial.print(sensorValue);
+  //Serial.println(voltageAD);
+  Serial.print(temperature);
+  //Serial.print(",");
+ // Serial.println("voltage");
+ // Serial.print(voltage);
+ // Serial.print(",");
+  //Serial.println("resistance");
+  //Serial.println(resistance);
 }
